32fd6dd24df42829967b3d487c5d56ed8862fb14
liang <1459539249@qq.com>
2020-05-29 16:05:25 +0800

    modify codes

--- lab9/lab9-exercise1.php
+++ lab9/lab9-exercise1.php
@@ -9,10 +9,16 @@
 <h1>PHP section (inside &lt;?php ... ?&gt; tags)</h1>
 <?php
 //this is a php comment IN tags (will not appear)
+if(date('L')===1){
 $remaining = 366 - date("z");
 echo "There are ". $remaining . " days left in the year";
 echo "<br>"; //notice we must echo tags in php.
-
+}
+else{
+    $remaining = 366 - date("z");
+    echo "There are ". $remaining . " days left in the year";
+    echo "<br>"; //notice we must echo tags in php.
+}
 ?>
 </body>
 </html>
